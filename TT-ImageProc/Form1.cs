d7cd1f446be4dbc18d54a42d7642aa7dfde3ae80
Martin Dubois <mdubois@kms-quebec.com>
2021-01-26 11:06:51 -0800

    TT-ImageProc - Initial

---
+++ TT-ImageProc/Form1.cs
@@ -0,0 +1,35 @@
+﻿
+// Author  Martin Dubois, P.Eng.
+// License https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
+// Product C-Sharp
+// File    TT-ImageProc/Form1.cs
+
+using System;
+using System.Windows.Forms;
+
+namespace TT_ImageProc
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            mCamera    = new Camera_USB();
+            mProcessor = new Processor();
+
+            mProcessor.Camera = mCamera;
+        }
+
+        private void cTakePicture_Click(object sender, EventArgs e)
+        {
+            cPictureBox.Image = mProcessor.TakePicture(cPictureBox.Size.Width, cPictureBox.Size.Height);
+        }
+
+        private ICamera   mCamera;
+        private Processor mProcessor;
+
+    }
+
+}
---
+++ TT-ImageProc/ICamera.cs
@@ -0,0 +1,17 @@
+﻿
+// Author  Martin Dubois, P.Eng.
+// License https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
+// Product C-Sharp
+// File    TT-ImageProc/ICamera.cs
+
+using System.Drawing;
+
+namespace TT_ImageProc
+{
+    interface ICamera
+    {
+
+        Image TakePicture();
+
+    }
+}
